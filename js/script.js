39b4d9cc9b3f9c8a4e3403544d9777508958c4d9
frendeeeeeeeee <stelgolends@gmail.com>
2021-04-09 17:38:22 +0300

    "add css and minimalize files"

--- js/script.js
+++ js/script.js
@@ -1,41 +1,88 @@
-let modalLogin = document.querySelector('.modal-login');
-let loginLink = document.querySelector('.login-link');
-let closeLogin = document.querySelector('.close-login');
+const modalLogin = document.querySelector('.modal-login');
+const loginLink = document.querySelector('.login-link');
+const closeLogin = document.querySelector('.close-login');
+const inputLogin = modalLogin.querySelector('[name="login"]');
+const inputPassword = modalLogin.querySelector('[name="password"]');
+const formLogin = modalLogin.querySelector('form');
+const modalMap = document.querySelector('.modal-map');
+const buttonMap = document.querySelector('.button-map');
+const closeMap = document.querySelector('.close-map');
 
-let modalMap = document.querySelector('.modal-map');
-let buttonMap = document.querySelector('.button-map');
-let closeMap = document.querySelector('.close-map');
+let storage = '';
+let isStorageSupport = true;
 
-
-
-let openModalLogin =() => {
-  modalLogin.classList.add('visible');
+try {
+  storage = localStorage.getItem('login');
+} catch (err) {
+  isStorageSupport = false;
 }
-let closeModalLogin = () => {
-  modalLogin.classList.remove('visible');
+
+
+const openModalLogin =() => {
+  modalLogin.classList.add('modal-visible');
 };
-loginLink.addEventListener('click', () => {
+const closeModalLogin = () => {
+  modalLogin.classList.remove('modal-visible');
+  modalLogin.classList.remove('modal-error');
+};
+
+const openModalMap = () => {
+  modalMap.classList.add('modal-visible');
+};
+const closeModalMap = () => {
+  modalMap.classList.remove('modal-visible');
+};
+
+
+
+loginLink.addEventListener('click', event => {
+  event.preventDefault();
   openModalLogin();
+  if(storage) {
+    inputLogin.value = storage;
+    inputPassword.focus();
+  } else {
+    inputLogin.focus();
+  }
+  
 });
+
 closeLogin.addEventListener('click', () => {
   closeModalLogin();
 });
 
+window.addEventListener('keydown', (event) => {
+  if(event.keyCode === 27) {
 
+    if(modalLogin.classList.contains('modal-visible')) {
+      event.preventDefault();
+      closeModalLogin();
+    }
+    if(modalMap.classList.contains('modal-visible')) {
+      event.preventDefault();
+      closeModalMap();
+    }
+  }
+});
 
-let openModalMap = () => {
-  modalMap.classList.add('visible');
-};
-let closeModalMap = () => {
-  modalMap.classList.remove('visible');
-};
+formLogin.addEventListener('submit', event => {
+  if(!inputLogin.value || !inputPassword.value) {
+    event.preventDefault();
+    modalLogin.classList.remove('modal-error');
+    modalLogin.offsetWidth = modalLogin.offsetWidth;
+    modalLogin.classList.add('modal-error');
+  } else {
+    if(isStorageSupport) {
+      localStorage.setItem('login', inputLogin.value);
+    }
+    
+  }
+});
 
 buttonMap.addEventListener('click', () => {
   openModalMap();
 });
+
 closeMap.addEventListener('click', () => {
   closeModalMap();
 });
-
-
-
